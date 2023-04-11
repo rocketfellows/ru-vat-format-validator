408db2133d8e6bebbbb82516b5238abfde7190c2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-12 00:10:46 +0400

    [CVFV-24] setup test.

--- tests/unit/RUVatFormatValidatorTest.php
+++ tests/unit/RUVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\RUVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class RUVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var RUVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new RUVatFormatValidator();
     }
 
     /**
