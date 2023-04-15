0b7a632b4a9b6d8dcb426aacd6e9127bc561abc7
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-15 16:22:25 +0400

    [CVFV-24] implemented validation organization vat number checksum.

--- src/RUVatFormatValidator.php
+++ src/RUVatFormatValidator.php
@@ -10,6 +10,8 @@ class RUVatFormatValidator extends CountryVatFormatValidator
     private const ORGANIZATION_VAT_NUMBER_PATTERN = '/^((\d{10}))?$/';
     private const INDIVIDUAL_VAT_NUMBER_PATTERN = '/^((\d{12}))?$/';
 
+    private const ORGANIZATION_VAT_NUMBER_CHECKSUM_MULTIPLIERS = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+
     protected function isValidFormat(string $vatNumber): bool
     {
         if (!$this->isValidFormatByGeneralPattern($vatNumber)) {
@@ -17,7 +19,7 @@ class RUVatFormatValidator extends CountryVatFormatValidator
         }
 
         if (!$this->isValidOrganizationVatNumberFormat($vatNumber)) {
-            return false;
+            return $this->isValidOrganizationVatNumberChecksum($vatNumber);
         }
 
         if (!$this->isValidIndividualVatNumberFormat($vatNumber)) {
@@ -45,5 +47,49 @@ class RUVatFormatValidator extends CountryVatFormatValidator
     private function isValidFormatByPattern(string $vatNumber, string $pattern): bool
     {
         return (bool) preg_match($pattern, $vatNumber);
+    }
+
+    private function isValidOrganizationVatNumberChecksum(string $vatNumber): bool
+    {
+        $vatNumberDigits = $this->getVatNumberDigits($vatNumber);
+        $vatNumberChecksumDigits = array_slice($vatNumberDigits, 0, 9);
+        $calculatedKey = $this->calculateKey(
+            $vatNumberChecksumDigits,
+            self::ORGANIZATION_VAT_NUMBER_CHECKSUM_MULTIPLIERS
+        );
+        $calculatedChecksum = $this->calculateChecksum($calculatedKey);
+
+        return ($calculatedChecksum === end($vatNumberDigits));
+    }
+
+    /**
+     * @param string $vatNumber
+     * @return int[]
+     */
+    private function getVatNumberDigits(string $vatNumber): array
+    {
+        return array_map(
+            static function (string $item): int {
+                return (int) $item;
+            },
+            str_split($vatNumber)
+        );
+    }
+
+    private function calculateKey(array $vatNumberDigits, array $vatNumberChecksumMultipliers): int
+    {
+        return array_sum(
+            array_map(
+                static function (int $vatNumberDigit, int $vatNumberChecksumMultiplier) {
+                    return $vatNumberDigit * $vatNumberChecksumMultiplier;
+                },
+                $vatNumberDigits, $vatNumberChecksumMultipliers
+            )
+        );
+    }
+
+    private function calculateChecksum(int $key): int
+    {
+        return ($key % 11) % 10;
     }
 }
