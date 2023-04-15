137fe3144f51f56f483ac688a7a2b75dfb6b6c7d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-15 15:47:24 +0400

    [CVFV-24] implemented validation organization vat number format by pattern.

--- src/RUVatFormatValidator.php
+++ src/RUVatFormatValidator.php
@@ -7,6 +7,7 @@ use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidator;
 class RUVatFormatValidator extends CountryVatFormatValidator
 {
     private const VAT_NUMBER_PATTERN = '/^((\d{12})|(\d{10}))?$/';
+    private const ORGANIZATION_VAT_NUMBER_PATTERN = '/^(([0-9]{10}))?$/';
 
     protected function isValidFormat(string $vatNumber): bool
     {
@@ -14,11 +15,25 @@ class RUVatFormatValidator extends CountryVatFormatValidator
             return false;
         }
 
-        return true;
+        if (!$this->isValidOrganizationVatNumberFormat($vatNumber)) {
+            return false;
+        }
+
+        return false;
     }
 
     private function isValidFormatByGeneralPattern(string $vatNumber): bool
     {
-        return (bool) preg_match(self::VAT_NUMBER_PATTERN, $vatNumber);
+        return $this->isValidFormatByPattern($vatNumber, self::VAT_NUMBER_PATTERN);
+    }
+
+    private function isValidOrganizationVatNumberFormat(string $vatNumber): bool
+    {
+        return $this->isValidFormatByPattern($vatNumber, self::ORGANIZATION_VAT_NUMBER_PATTERN);
+    }
+
+    private function isValidFormatByPattern(string $vatNumber, string $pattern): bool
+    {
+        return (bool) preg_match($pattern, $vatNumber);
     }
 }
